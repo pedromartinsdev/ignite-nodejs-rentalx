8efa75e3f43d65edecdb2574636ee3c010d0e16b
pedro <pedrohenriquemartins@live.com>
2021-12-22 23:37:33 -0300

    Upload project

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+import { Category } from "../model/Category";
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+
+
+  return response.status(201).json({ category });
+});
+
+export { categoriesRoutes };
