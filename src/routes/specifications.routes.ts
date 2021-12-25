d0552eebaa455c3f4999f00d6a95f914bf53fcd2
pedro <pedrohenriquemartins@live.com>
2021-12-25 03:31:05 -0300

    Chapter II, module IV, class IV

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,14 @@
+import { response, Router } from "express";
+
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationsRoutes = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (request, response) => {
+
+});
+
+export { specificationsRoutes };
