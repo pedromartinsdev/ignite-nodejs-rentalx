d6b0dd79579a9ce5f761a88488a0407586040745
pedro <pedrohenriquemartins@live.com>
2022-02-11 21:53:27 -0300

    application refactor

--- src/modules/accounts/infra/typeorm/repositories/UsersRepostory.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepostory.ts
@@ -1,8 +1,9 @@
 import { getRepository, Repository } from "typeorm";
 
-import { ICreateUserDTO } from "../../dtos/ICreateUSerDTO";
-import { User } from "../../entities/User";
-import { IUsersRepository } from "../IUsersRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+
+import { ICreateUserDTO } from "../../../dtos/ICreateUSerDTO";
+import { User } from "../entities/User";
 
 class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,14 +1,15 @@
 import "reflect-metadata";
-import "./database";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "@shared/container";
-import { AppError } from "@errors/AppError";
+import "@shared/infra/typeorm";
 
+import "@shared/container";
+import { AppError } from "@shared/errors/AppError";
+
+import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
-import swaggerFile from "./swagger.json";
 
 const app = express();
 
