17a1c25ffababbda1647b0168c6bcc45a4dac23e
pedro <pedrohenriquemartins@live.com>
2022-03-03 21:29:58 -0300

    add car images use case

---
+++ src/modules/cars/infra/typeorm/repositories/CarsImagesRepository.ts
@@ -0,0 +1,26 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+
+import { CarImage } from "../entities/CarImage";
+
+class CarsImagesRepository implements ICarsImagesRepository {
+  private reposiroty: Repository<CarImage>;
+
+  constructor() {
+    this.reposiroty = getRepository(CarImage);
+  }
+
+  async create(car_id: string, image_name: string): Promise<CarImage> {
+    const carImage = this.reposiroty.create({
+      car_id,
+      image_name,
+    });
+
+    await this.reposiroty.save(carImage);
+
+    return carImage;
+  }
+}
+
+export { CarsImagesRepository };
